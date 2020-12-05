0cf8822af24f4fc81356fdef6df03c9037827a7d
Walter Setti <walter.setti@iit.it>
2020-12-05 12:00:07 +0100

    Home Page

---
+++ Sezione_Aureawe/Sezione_Aureawe/Initial.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Sezione_Aureawe
+{
+    partial class Initial
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labLuda = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labLuda
+            // 
+            this.labLuda.AutoSize = true;
+            this.labLuda.BackColor = System.Drawing.Color.Transparent;
+            this.labLuda.Font = new System.Drawing.Font("Snap ITC", 50.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.labLuda.ForeColor = System.Drawing.Color.Gold;
+            this.labLuda.Location = new System.Drawing.Point(16, -30);
+            this.labLuda.Margin = new System.Windows.Forms.Padding(3);
+            this.labLuda.Name = "labLuda";
+            this.labLuda.Size = new System.Drawing.Size(1194, 86);
+            this.labLuda.TabIndex = 2;
+            this.labLuda.Text = "UNA SEZIONE TUTTA D\'ORO";
+            this.labLuda.Click += new System.EventHandler(this.labLuda_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Font = new System.Drawing.Font("Snap ITC", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Blue;
+            this.button1.Location = new System.Drawing.Point(781, 508);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 41);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "INIZIA!";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Sezione_Aureawe.Properties.Resources.group1;
+            this.pictureBox1.Location = new System.Drawing.Point(485, 306);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(365, 196);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Initial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labLuda);
+            this.Name = "Initial";
+            this.Size = new System.Drawing.Size(1417, 701);
+            this.Load += new System.EventHandler(this.Initial_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labLuda;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Sezione_Aureawe/Sezione_Aureawe/Main.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Media;
+using System.IO;
+using System.Threading;
+
+
+namespace Sezione_Aureawe
+{
+    public partial class Main : Form
+    {
+        public static readonly string appPath = Path.GetDirectoryName(Application.ExecutablePath);
+        public static readonly string resourcesPath = Path.GetDirectoryName(Application.ExecutablePath) + "\\resources";
+        private const string background_image = "galaxy.jpg";
+        private UserControl currUC = null;
+        public int interaction;
+        public SoundPlayer player = null;
+        public Main()
+        {
+            InitializeComponent();
+            initial1.parentForm = this;
+            initial1.Visible = false;
+            home();
+            BackgroundImageLayout = ImageLayout.Stretch;
+            BackgroundImage = Image.FromFile(resourcesPath + "\\" + background_image);
+           
+        }
+
+        public void home()
+        {
+            if (currUC != null) currUC.Visible = false;
+            initial1.Show();
+            currUC = initial1;
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            Size size = this.Size; 
+            initial1.setPos(size.Width, size.Height);
+        }
+
+        private void initial1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
